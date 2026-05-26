9fd8ef589392c731cde4db8b50b01170a5d5c4ef
Github73840134 <35036025+Github73840134@users.noreply.github.com>
2026-05-26 13:32:51 -0400

    Version 2

--- app/app.py
+++ app/app.py
@@ -2,4 +2,4 @@ import os, sys
 os.chdir(os.path.dirname(__file__))
 sys.path.insert(0,os.path.join(os.path.dirname(os.getcwd()), "libs"))
 import subprocess
-subprocess.run(f"../core/python.exe XumoPlayApp.py {' '.join(sys.argv[1:])}")
+subprocess.run(f"../core/pythonw.exe XumoPlayApp.py {' '.join(sys.argv[1:])}")
